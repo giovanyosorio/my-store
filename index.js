56cb05ccf9869d78d784523a279289ca78193cb6
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-05-26 15:58:37 -0500

    Routing con Express.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+
+const express =require('express');
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello woorld!');
+} );
+
+app.get('/products', (req, res) => {
+  res.json({name: 'iphone', price: 1000, quantity: 10});
+} );
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
