b8185f6d24600d22bec99592106f051ccf9d650a
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-05-26 18:59:29 -0500

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 
-const express =require('express');
+const express = require('express');
 
 const app = express();
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello woorld!');
-} );
+  res.send('Hello woorld!');
+});
 
 app.get('/products', (req, res) => {
-  res.json({name: 'iphone', price: 1000, quantity: 10});
-} );
+  res.json([
+    { name: 'iphone',
+     price: 1000,
+      quantity: 10
+     },{
+      name: 'samsung',
+      price: 900,
+      quantity: 10
+     }]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'iphone',
+    price: 1000,
+    quantity: 10,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId,productId}=req.params
+  res.json({
+    categoryId,
+    productId,
+  })
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
